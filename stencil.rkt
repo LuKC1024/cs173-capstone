df8783517c6076712e4d702200028085b85d5ec2
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2022-11-06 11:45:11 -0500

    initial

---
+++ stencil.rkt
@@ -0,0 +1,77 @@
+#lang racket
+(require redex)
+(define-language SMoLest
+  ; an expression
+  [e ::=
+     ; variable
+     x
+     ; value
+     v
+     ; addition
+     (+ e ...)
+     ; application
+     (e e ...)
+     ; lamda
+     (lambda (x ...) e)]
+  ; values
+  [v ::=
+     number
+     (lambda (x ...) e σ)]
+  ; environment
+  [σ ::= ((x v) ...)]
+  ; evaluation context
+  ; used in `eval`
+  ; these are the compound forms of `e`
+  ; with one sub expression replaced by a hole, `C`.
+  [C ::=
+     (v ... C e ...)
+     (+ v ... C e ...)
+     hole]
+  [x ::= variable-not-otherwise-mentioned])
+; extend the environment with bindings to values
+(define-metafunction SMoLest
+  extend : ((x any) ...)  (x ...) (any ...) -> ((x any) ...)
+  [(extend ((x any) ...) (x_1 ...) (any_1 ...))
+   ((x_1 any_1) ... (x any) ...)])
+; an example of extend
+(test-equal
+ (term (extend ((a 1)) (b c) (2 3)))
+ (term ((b 2) (c 3) (a 1))))
+; lookup a value in the environment
+(define-metafunction SMoLest
+  lookup : ((any_k any_v) ...) any_n -> any
+  [(lookup ((any_k any_v) ...) any_n)
+   ,(letrec ([maybe-v (assoc (term any_n) (term ((any_k any_v) ...)))])
+      (if maybe-v
+          (last maybe-v)
+          (error 'unbound "~e" (term any_n))))])
+; an example of lookup
+(test-equal
+ (term (lookup ((a 1)) a))
+ 1)
+; `eval` reduction relation
+(define eval
+  (reduction-relation
+   SMoLest
+   #:domain (e σ)
+   ; +
+   (--> [(in-hole C (+ number ...)) σ]
+        [(in-hole C ,(apply + (term (number ...)))) σ])
+   ; lookup
+   (--> [(in-hole C x) σ]
+        [(in-hole C (lookup σ x)) σ])
+   ; lambda
+   (--> [(in-hole C (lambda (x ...) e)) σ]
+        [(in-hole C (lambda (x ...) e σ)) σ])
+   ; application
+   (--> [(in-hole C ((lambda (x ...) e σ) v ...)) _]
+        [(in-hole C e) (extend σ (x ...) (v ...))])
+   ))
+; example of evaluating +
+(test-->> eval
+          ; note the `()`, which denotes the empty environment
+          (term [(+ (+ 1 2) (+ 3 4))
+                 ()])
+          (term [10
+                 ()]))
+(test-results)
