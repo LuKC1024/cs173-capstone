208c1ed56cc82ed912b5d9f94763a02121f90d61
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2022-12-07 17:07:14 -0500

    Update stencil.rkt

--- stencil.rkt
+++ stencil.rkt
@@ -1,19 +1,25 @@
 #lang racket
 (require redex)
+
 (define-language SMoLest
+  ; observable
+  [o ::=
+     number
+     (function)]
   ; expression
   [e ::=
-     ; variable
+     ; variable reference
      x
      ; value
      v
      ; addition
      (+ e ...)
-     ; application
+     ; function call
      (e e ...)
      ; lambda
      (lambda (x ...) e)]
   ; value
+  ; TODO: You are welcomed to change the definition of values
   [v ::=
      number
      (lambda (x ...) e σ)]
@@ -34,41 +40,36 @@
   ; variable
   [x ::= variable-not-otherwise-mentioned])
 
-
-
 ; extend the environment with bindings to values
 (define-metafunction SMoLest
-  extend : ((x any) ...)  (x ...) (any ...) -> ((x any) ...)
-  [(extend ((x any) ...) (x_1 ...) (any_1 ...))
-   ((x_1 any_1) ... (x any) ...)])
+  extend : ((x v) ...)  (x ...) (v ...) -> ((x v) ...)
+  [(extend ((x_1 v_1) ...) (x_2 ...) (v_2 ...))
+   ((x_2 v_2) ... (x_1 v_1) ...)])
 
 ; an example of extend
 (test-equal
  (term (extend ((a 1)) (b c) (2 3)))
  (term ((b 2) (c 3) (a 1))))
 
-
-
 ; lookup a value in the environment
 (define-metafunction SMoLest
-  lookup : ((any_k any_v) ...) any_n -> any
-  [(lookup ((any_k any_v) ...) any_n)
-   ,(letrec ([maybe-v (assoc (term any_n) (term ((any_k any_v) ...)))])
+  lookup : σ x -> v
+  [(lookup σ x)
+   ,(letrec ([maybe-v (assoc (term x) (term σ))])
       (if maybe-v
           (last maybe-v)
-          (error 'unbound "~e" (term any_n))))])
+          (error 'unbound "~e" (term x))))])
 
 ; an example of lookup
 (test-equal
  (term (lookup ((a 1)) a))
  1)
 
-
-
 ; `eval` reduction relation
 (define eval
   (reduction-relation
    SMoLest
+   ; TODO: You are welcomed to change the domain and other parts of this file accordingly
    #:domain (e σ s)
    ; +
    (--> [(in-hole C (+ number ...)) σ s]
@@ -77,6 +78,7 @@
    (--> [(in-hole C x) σ s]
         [(in-hole C (lookup σ x)) σ s])
    ; lambda
+   ; TODO: You are welcomed to change the lambda case
    (--> [(in-hole C (lambda (x ...) e)) σ s]
         [(in-hole C (lambda (x ...) e σ)) σ s])
    ; application
@@ -84,7 +86,14 @@
         [e (extend σ_1 (x ...) (v ...)) (C σ_2 s)])
    ; return
    (--> [v σ_1 (C σ_2 s)]
-        [(in-hole C v) σ_2 s])))
+        [(in-hole C v) σ_2 s])
+   ; TODO: Part 1. implement `eq?`
+   ; TODO: Part 1. implement `if`
+   ))
+
+; TODO: Part 2. implement `let`
+; TODO: Part 2. implement `let*`
+; TODO: Part 2. implement `begin`
 
 ; example of evaluating +
 (test-->> eval
@@ -96,20 +105,59 @@
           (term [10
                  ()
                  ()]))
-; example of function calls
-(test-->> eval
-          (term [(((lambda (x) (lambda (y) (+ x y))) 2) 3)
-                 ()
-                 ()])
-          (term [5
-                 ()
-                 ()]))
-; lexical scope
-(test-->> eval
-          (term [((lambda (x) (+ ((lambda (x) x) 3) x)) 1)
-                 ()
-                 ()])
-          (term [4
-                 ()
-                 ()]))
+; You can use `stepper` to debug programs
+; TODO: try uncomment the following expression or run it in the REPL
+#;
+(stepper eval
+         ; The first `()` denotes the empty environment
+         ; The second `()` denotes the empty stack
+         (term [(+ (+ 1 2) (+ 3 4))
+                ()
+                ()]))
+
+
+(define-metafunction SMoLest
+  observe : v -> o
+  [(observe number)
+   number]
+  ;; TODO: you are welcomed to change the lambda case
+  [(observe (lambda (x ...) e σ))
+   (function)])
+
+(define-metafunction SMoLest
+  run : e -> o
+  [(run e)
+   (observe v)
+   (where ((v σ s)) ,(apply-reduction-relation* eval (term [e () ()])))])
+
+(test-equal (term (run (+ (+ 1 2) (+ 3 4))))
+            (term 10))
+(test-equal (term (run (((lambda (x) (lambda (y) (+ x y))) 2) 3)))
+            (term 5))
+(test-equal (term (run ((lambda (x) (+ ((lambda (x) x) 3) x)) 1)))
+            (term 4))
+; TODO: uncomment and make sure your `eq?` works as we expected
+#|
+(test-equal (term (run (eq? (lambda (x) x)
+                            (lambda (x) x))
+                       ))
+            (term #f))
+(test-equal (term (run (let ([f (lambda (x) x)])
+                         (eq? f f))
+                       ))
+            (term #t))
+(test-equal (term (run (let* ([f (lambda (x) x)]
+                              [g f])
+                         (eq? ((lambda () g)) f))
+                       ))
+            (term #t))
+|#
+; TODO: uncomment and make sure your `if` works as we expected
+#|
+(test-equal (term (run (if (eq? 2 3))
+                           ((lambda (x) (x x)) (lambda (x) (x x)))
+                           42))
+            (term 42))
+|#
+;; TODO: You are welcomed to add more tests here.
 (test-results)
